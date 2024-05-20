f31aafbacf13ca045b6cfc6c77426e3b844d57cf
Tamer autlev <96975519+Tamer-Autlev@users.noreply.github.com>
2024-05-20 14:10:03 +0300

    Update Form.jsx

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -33,9 +33,9 @@ const Contact = ({lang}) => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Rami",
+          to_name: "Mr.Ymaf",
           from_email: form.email,
-          to_email: "rami-lash@outlook.com",
+          to_email: "info@harvest-gp.com",
           message: form.message,
           from_number: form.number,
         },
@@ -130,7 +130,7 @@ const Contact = ({lang}) => {
         onChange={handleChange}
       />
       <button
-        className={`form-btn ${send ? "active-btn" : ""} max-sm:w-[90%] mt-3`}
+        className={`form-btn ${send ? "active-btn" : ""} ${send?``:`max-sm:w-[90%]`} mt-3`}
         type="submit"
         onClick={sendSubmit}
       >
